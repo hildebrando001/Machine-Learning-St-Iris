74f2e685bf0f1e7b7a53b30365d1fdab4b6e3ee1
José Hildebrando <jose.hildebrando@gmail.com>
2021-04-20 19:21:13 -0300

    show iris image

--- iris.py
+++ iris.py
@@ -12,10 +12,10 @@ This app predict Iris flower type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width  = st.sidebar.slider('Sepal width',  2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width  = st.sidebar.slider('Petal width',  0.1, 2.5, 0.2)
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 6.2)
+    sepal_width  = st.sidebar.slider('Sepal width',  2.0, 4.4, 3.1)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 3.9)
+    petal_width  = st.sidebar.slider('Petal width',  0.1, 2.5, 1.3)
     data = {'Sepal length':sepal_length,
             'Sepal width' :sepal_width,
             'Petal length':petal_length,
@@ -23,10 +23,12 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
-
 df = user_input_features()
 st.subheader('User input parameters')
 st.write(df)
+
+# sidebar image
+st.sidebar.image("iris_sepal_petal.png")
 
 iris = datasets.load_iris() # load the iris dataset
 X = iris.data
@@ -39,7 +41,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 # defining columns layout
-col1, col2 = st.beta_columns(2) # ([1, 2])
+col1, col2 = st.beta_columns([1, 2])   # (2)
 
 # col1 content
 col1.subheader('Class labels')
@@ -51,8 +53,26 @@ col1.write(iris.target_names[prediction])
 col1.subheader('Prediction probability')
 col1.write(prediction_proba)
 
-# col2 content
-col2.subheader(f"Iris {iris.target_names[prediction]}")
+# col2 content - Image title
+for tn in iris.target_names:
+    # col2.write(tn)
+    if iris.target_names[prediction] == tn:
+        col2.success(f"Iris {tn}")
+
+img_setosa     = "setosa.jpg"
+img_versicolor = "versicolor.jpg"
+img_virginica  = "virginica.jpg"
+
+
+# show image
+if iris.target_names[prediction] == "setosa":
+    img_link = img_setosa
+elif iris.target_names[prediction] == "versicolor":
+    img_link = img_versicolor
+else:
+    img_link = img_virginica
+
+col2.image(img_link)
 
 
 # st.write(dir(st.sidebar))
